b59c0100bbf0be74c8d64f1c44993a73c166d3fc
victor moreno <csuvictormoreno2@gmail.com>
2024-03-11 22:13:46 -0700

    finished line through and delete on dblclick

--- todo.js
+++ todo.js
@@ -1,13 +1,35 @@
 let inputBoxContainer = document.getElementById("inputBox");
 
-let taskBoxContainer = document.getElementById("itemList");
+let taskContainer = document.getElementById("itemList");
 
 function addTask(){
     var itemList = document.getElementById("inputBox").value;
     if (itemList.trim() !==""){
-        var taskBoxContainer = document.createElement('li');
-        taskBoxContainer.appendChild(document.createTextNode(itemList));
-        document.getElementById("itemList").appendChild(taskBoxContainer);
+        var taskContainer = document.createElement('li');
+        taskContainer.appendChild(document.createTextNode(itemList));
+        document.getElementById("itemList").appendChild(taskContainer);
         document.getElementById("inputBox").value='';
+        saveData();
+
+        taskContainer.addEventListener('click',function(){
+            taskContainer.style.textDecoration = "line-through"
+            taskContainer.style.color = "red";
+            saveData();
+        })
+
+        taskContainer.addEventListener('dblclick',function(){
+            taskContainer.parentNode.removeChild(taskContainer);
+            saveData();
+        })
+
     }
+}
+
+function bluffAlert(){
+    var theBluff = "So you've called my bluff... I can't do that yet, but thanks for believing in me! T~T ";
+    alert(theBluff);
+}
+
+function saveData(){
+    localStorage.setItem("data", itemList.innerHTML)
 }
